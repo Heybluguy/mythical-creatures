0b014c822385e5976371794f1a8ad6514304bc83
Luis  Senior <heybluguy@yahoo.com>
2019-05-27 23:15:16 -0500

    Hobbit finished

--- exercises/hobbit.js
+++ exercises/hobbit.js
@@ -7,12 +7,17 @@ class Hobbit {
     this.old = false;
     this.isShort = true;
     this.hasRing = false;
+    this.hasRingy();
   }
 
   celebrateBirthday() {
     this.age++;
-    (this.age >= 33 ? this.adult = true : this.adult = false);
-    (this.age >= 101 ? this.old = true : this.old = false);
+    if (this.age >= 33) { this.adult = true };
+    if (this.age >= 101) { this.old = true };
+  }
+
+  hasRingy() {
+    if (this.name === 'Frodo') {this.hasRing = true };
   }
 }
 
